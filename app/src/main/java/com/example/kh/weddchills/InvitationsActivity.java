d1e6536c975c618dd59e7d1540b950d85d1e28d4
KarolinaHajzer <paczeg@gmail.com>
2019-06-01 14:15:00 +0200

    Invitation Activity without errors

--- app/src/main/java/com/example/kh/weddchills/InvitationsActivity.java
+++ app/src/main/java/com/example/kh/weddchills/InvitationsActivity.java
@@ -1,7 +1,5 @@
 package com.example.kh.weddchills;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,40 +8,62 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+
 
 public class InvitationsActivity extends AppCompatActivity {
 
     EditText invitation_edit_text;
     Button mInvitationSaveButton;
-    SharedPreferences saved_notes;
-    String saved_notes_before;
+    TextView invitation_text_view;
+    static final int READ_BLOCK_SIZE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invitations);
 
-        saved_notes = getSharedPreferences("prefID", Context.MODE_PRIVATE);
-
-        String name = saved_notes.getString("nameKey","No name");
-
-        TextView label = findViewById(R.id.labelID);
-        label.setText(name);
         invitation_edit_text = findViewById(R.id.invitation_edit_text_id);
+        invitation_text_view = findViewById(R.id.labelID);
         mInvitationSaveButton = findViewById(R.id.invitation_save_button);
 
-        // add-write text into file
-        try {
-            FileOutputStream fileout=openFileOutput("mytextfile.txt", MODE_PRIVATE);
-            OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
-            outputWriter.write(invitation_edit_text.getText().toString());
-            outputWriter.close();
+        File f = getFileStreamPath("mytextfile.txt");
+        if(f.length()>0){
+            try {
+                FileOutputStream fileout=openFileOutput("mytextfile.txt", MODE_PRIVATE);
+                OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
+                outputWriter.write("0");
+                outputWriter.close();
 
-            //display file saved message
-            Toast.makeText(getBaseContext(), "File saved successfully!",
-                    Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        try {
+            FileInputStream fileIn=openFileInput("mytextfile.txt");
+
+                InputStreamReader InputRead = new InputStreamReader(fileIn);
+
+                char[] inputBuffer = new char[READ_BLOCK_SIZE];
+                String s = "";
+                int charRead;
+
+                while ((charRead = InputRead.read(inputBuffer)) > 0) {
+                    // char to string conversion
+                    String readstring = String.copyValueOf(inputBuffer, 0, charRead);
+                    s += readstring;
+                }
+                InputRead.close();
+                invitation_text_view.setText(s);
+                //Toast.makeText(getBaseContext(), s, Toast.LENGTH_SHORT).show();
+
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -51,26 +71,22 @@ public class InvitationsActivity extends AppCompatActivity {
 
 
         mInvitationSaveButton.setOnClickListener(new View.OnClickListener() {
-                                                     @Override
-                                                     public void onClick(View v) {
-             //get reference to TextView
-             TextView label = findViewById(R.id.labelID);
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream fileout=openFileOutput("mytextfile.txt", MODE_APPEND);
+                    OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
+                    outputWriter.append(invitation_edit_text.getText().toString());
+                    outputWriter.close();
 
-             //get references to Name and Age EditTexts
-             EditText invitation_edit_text = findViewById(R.id.invitation_edit_text_id);
+                    //display file saved message
+                    Toast.makeText(getBaseContext(), "File saved successfully!",
+                            Toast.LENGTH_SHORT).show();
 
-             //set up SharedPreferences
-             saved_notes = getSharedPreferences("prefID", Context.MODE_PRIVATE);
-             saved_notes_before = saved_notes.getString("prefID",null);
-             SharedPreferences.Editor editor = saved_notes.edit();
-             editor.putString("nameKey", saved_notes_before + invitation_edit_text.getText().toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
-             editor.apply();
-             label.setText("Saved");
-         }
-     }
-    );
-    }
-}
-
-
+            }
+        });
+    }}
